59804f9c0798a2a25334acac0eeec4061e82b0be
lonelynight-dark <52921466+trantrunghieu0409@users.noreply.github.com>
2022-04-05 18:58:59 +0700

    add AppConfig Class
    This class is to get data from App.Config file

---
+++ ProjectMyShop/ProjectMyShop/AppConfig.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMyShop
+{
+    internal class AppConfig
+    {
+        public static string Server = "Server";
+        public static string Instance = "Instance";
+        public static string Database = "Database";
+        public static string Username = "Username";
+        public static string Password = "Password";
+        public static string Entropy = "Entropy";
+
+        public static string? GetValue(string key)
+        {
+            string? value = ConfigurationManager
+                .AppSettings[key];
+            return value;
+        }
+
+        public static void SetValue(string key, string value)
+        {
+            var configFile = ConfigurationManager
+            .OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            settings[key].Value = value;
+
+            configFile.Save(ConfigurationSaveMode.Minimal);
+        }
+
+        public static string? ConnectionString()
+        {
+            string? result = "";
+
+            var builder = new SqlConnectionStringBuilder();
+            string? server = AppConfig.GetValue(AppConfig.Server);
+            string? instance = AppConfig.GetValue(AppConfig.Instance);
+            string? database = AppConfig.GetValue(AppConfig.Database);
+            string? username = AppConfig.GetValue(AppConfig.Username);
+            string? password = AppConfig.GetValue(AppConfig.Password);
+
+            builder.DataSource = $"{server}\\{instance}";
+            builder.InitialCatalog = database;
+            builder.IntegratedSecurity = true;
+            builder.ConnectTimeout = 3; // s
+
+            result = builder.ToString();
+            return result;
+        }
+
+        static String getPassword()
+        {
+            var cypherText = AppConfig.GetValue(AppConfig.Password);
+            var cypherTextInBytes = Convert.FromBase64String(cypherText!);
+
+            var entropyText = AppConfig.GetValue(AppConfig.Entropy);
+            var entropyTextInBytes = Convert.FromBase64String(entropyText);
+
+            var passwordInBytes = ProtectedData.Unprotect(cypherTextInBytes,
+                entropyTextInBytes, DataProtectionScope.CurrentUser);
+            var password = Encoding.UTF8.GetString(passwordInBytes);
+
+            return password;
+        }
+
+        static void setPassword(String password)
+        {
+
+            var passwordInBytes = Encoding.UTF8.GetBytes(password);
+
+            var entropy = new byte[20];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(entropy);
+            }
+            var entropyBase64 = Convert.ToBase64String(entropy);
+
+            var cypherText = ProtectedData.Protect(passwordInBytes, entropy,
+                DataProtectionScope.CurrentUser);
+            var cypherTextBase64 = Convert.ToBase64String(cypherText);
+
+            AppConfig.SetValue(AppConfig.Password, cypherTextBase64);
+            AppConfig.SetValue(AppConfig.Entropy, entropyBase64);
+        }
+    }
+}
