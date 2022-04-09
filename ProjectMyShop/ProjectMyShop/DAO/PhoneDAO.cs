0766d3b08a42149aa94d88f2947fb3060d159e43
Lê Quang Tấn Long <63658369+lequangtanlong@users.noreply.github.com>
2022-04-10 02:53:35 +0700

    resolve conflicts

--- ProjectMyShop/ProjectMyShop/DAO/PhoneDAO.cs
+++ ProjectMyShop/ProjectMyShop/DAO/PhoneDAO.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectMyShop.DTO;
 
 namespace ProjectMyShop.DAO
 {
@@ -23,6 +24,38 @@ namespace ProjectMyShop.DAO
             }
             reader.Close();
             return result;
+        }
+
+        public List<Phone> GetTop5OutStock()
+        {
+            var sql = "select top(5) * from Phone order by stock desc";
+            var command = new SqlCommand(sql, _connection);
+            var reader = command.ExecuteReader();
+
+            List<Phone> list = new List<Phone>();
+            while (reader.Read())
+            {
+                var ID = (int)reader["ID"];
+                var PhoneName = (String)reader["PhoneName"];
+                var Manufacturer = (String)reader["Manufacturer"];
+
+                var SoldPrice = (int)(decimal)reader["SoldPrice"];
+                //var SoldPrice = (int)reader["SoldPrice"];
+                var Stock = (int)reader["Stock"];
+
+                Phone phone = new Phone()
+                {
+                    ID = ID,
+                    PhoneName = PhoneName,
+                    Manufacturer = Manufacturer,
+                    SoldPrice = SoldPrice,
+                    Stock = Stock,
+                };
+                if (phone.PhoneName != "")
+                    list.Add(phone);
+            }
+            reader.Close();
+            return list;
         }
     }
 }
