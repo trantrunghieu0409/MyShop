ab2c2f7cf9c06a31f0a78960739b0802d8008bac
lonelynight-dark <52921466+trantrunghieu0409@users.noreply.github.com>
2022-04-09 16:43:48 +0700

    update layout manage orders

---
+++ ProjectMyShop/ProjectMyShop/Converter/NewRowIndicatorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ProjectMyShop.Converter
+{
+    public class NewRowIndicatorConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return "";
+            if (value.GetType().FullName == "MS.Internal.NamedObject")
+                return "*";
+            return "";
+        }
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // this function is not supposed to be called
+            throw new Exception();
+        }
+    }
+}
